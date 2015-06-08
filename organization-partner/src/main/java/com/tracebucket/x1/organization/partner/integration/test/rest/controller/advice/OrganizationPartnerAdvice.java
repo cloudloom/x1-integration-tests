d4965205ffc82d8f1a0c622154bd6ac5b68c08d7
sadath <ssmdrepo@gmail.com>
2015-06-08 11:25:42 +0530

    Organization Partner Integration. Get Organizations Partners Controller Test Case Implemented.

---
+++ organization-partner/src/main/java/com/tracebucket/x1/organization/partner/integration/test/rest/controller/advice/OrganizationPartnerAdvice.java
@@ -0,0 +1,20 @@
+package com.tracebucket.x1.organization.partner.integration.test.rest.controller.advice;
+
+import com.tracebucket.x1.organization.api.rest.exception.OrganizationException;
+import com.tracebucket.x1.organization.partner.integration.test.rest.exception.OrganizationPartnerException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by sadath on 28-Apr-15.
+ */
+@ControllerAdvice
+public class OrganizationPartnerAdvice {
+    @ExceptionHandler(OrganizationException.class)
+    @ResponseBody
+    public ResponseEntity<String> handleOrganizationPartnerException(OrganizationPartnerException ex) {
+        return new ResponseEntity<String>(ex.getMessage(), ex.getHttpStatus());
+    }
+}
